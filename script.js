6ac1b9790326572cf0195d0db5c4f251eaa86a7e
santiagodelpercioR <santiagodelpercio@gmail.com>
2024-10-01 09:32:13 -0300

    Agrego funcionalidad cambio tamanio grilla

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-function createGrid(){
+function createGrid(h = 16, w = 16){
     let div;
     let columna;
-    for(let x = 1; x <= 16; x++){
+    for(let x = 1; x <= w; x++){
         columna = document.createElement("div");
-        for(let i = 1; i  <= 16; i++){
+        for(let i = 1; i  <= h; i++){
             div = document.createElement("div");
+            setColorFunctionality(div);
             columna.appendChild(div);
         }
         container.appendChild(columna);
     }
 }
 
+function setColorFunctionality(element){
+    element.addEventListener("mouseover", (event) => {
+        console.log("cambiar");
+        element.style.backgroundColor = "red";
+    });
+    
+}
+
+function deleteGrid(){
+    while(container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+}
+
 const container = document.querySelector(".grid-container");
+const body = document.querySelector("body");
+const btn = document.createElement("button");
+
+body.insertBefore(btn, container);
+
+btn.textContent = "Change grid";
+
+btn.addEventListener("click", (event) => {
+    let height = prompt("Height: ");
+    let width = prompt("Width: ");
+    deleteGrid();
+    createGrid(height, width);
+});
 
 createGrid();
+
 
